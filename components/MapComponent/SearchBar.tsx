60d883597ec97b5cbe263c2de3d720c43eeb105b
sarikamahboob <sarika.mahboob1234@gmail.com>
2025-08-28 20:39:49 +0600

    feat: updated searchbar function

--- components/MapComponent/SearchBar.tsx
+++ components/MapComponent/SearchBar.tsx
@@ -69,7 +69,7 @@ const SearchBar: React.FC<SearchBarProps> = ({
           },
           headers: {
             'accept': 'application/json',
-            'X-API-KEY': `${process.env.NEXT_PUBLIC_AUTOCOMPLETE_API_KEY}`,
+            'X-API-KEY': process.env.NEXT_PUBLIC_AUTOCOMPLETE_API_KEY || '',
           },
         }
       );
